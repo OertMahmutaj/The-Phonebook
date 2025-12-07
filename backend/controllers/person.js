eb85f258d98c103bdc432c504747ecc146519eae
Oert Mahmutaj <oert64@email.com>
2025-12-07 10:51:08 +0100

    Refactor backend structure: separate app, routes, and middleware; implement logging and error handling

---
+++ backend/controllers/person.js
@@ -0,0 +1,64 @@
+const personRouter = require('express').Router()
+const Person = require('../models/person')
+
+personRouter.get('/', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+
+personRouter.get('/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+personRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+personRouter.delete('/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+personRouter.put('/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
+module.exports = personRouter
--- backend/models/person.js
+++ backend/models/person.js
@@ -1,19 +1,4 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
-mongoose.set('strictQuery', false)
-
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
-
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const personSchema = new mongoose.Schema({
   name: {
